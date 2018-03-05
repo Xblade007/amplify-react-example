b0ab0f5cdb06d5b9ad30aedbfd781eefd0638ca0
Stephen Jenkinson <stephen.jenkinson@gmail.com>
2018-03-05 14:39:01 +1000

    Configure and enable authentication with aws-amplify

--- README.md
+++ README.md
@@ -8,3 +8,21 @@ yarn start
 ```
 
 
+### Steps creating this app
+
+#### Created skeleton app using create-react-app
+```
+    npx create-react-add amplify-react-example 
+    cd amplify-react-example 
+    yarn add redux redux-saga prop-types aws-amplify aws-amplify-react semantic-ui-react
+```
+[commit](https://github.com/spjenk/amplify-react-example/commit/6f3a3d7b1a8b2d09bc5c0a198c5022f90a4ae362)
+
+
+
+#### Enabled authentication with aws-amplify 
+Configured and enabled aws-amplify within app.js  
+
+
+
+
--- src/App.js
+++ src/App.js
@@ -2,6 +2,20 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Amplify from 'aws-amplify';
+import { withAuthenticator} from 'aws-amplify-react';
+
+//configure and register aws amplify
+const amplify_config = {
+  Auth: {
+    aws_cognito_identity_pool_id: 'ap-southeast-2:fcd83fb3-1c99-4002-b406-80ef5245bf8b',
+    aws_user_pools_id: 'ap-southeast-2_GOWBVxrJa',
+    aws_user_pools_web_client_id: 'v2oi7kab1irvg83265cisg515',
+    aws_cognito_region: 'ap-southeast-2'
+  }
+};
+Amplify.configure(amplify_config);
+
 class App extends Component {
   render() {
     return (
@@ -18,4 +32,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default withAuthenticator(App, true);
